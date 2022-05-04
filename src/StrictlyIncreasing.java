fe86548bbcb8fb9d369f896527ea4d70de7d7bdb
Mary White <mary.a.white28@gmail.com>
2022-05-03 21:50:09 -0400

    solved stricly increasing with help from github

--- src/StrictlyIncreasing.java
+++ src/StrictlyIncreasing.java
@@ -16,57 +16,87 @@ public class StrictlyIncreasing {
 //        return answer;
 //    }
 
-    public static boolean isStrictlyIncreasing(int[] arr) {
-        int modify = 0;
-        int n = arr.length;
+    //the one below doesn't pass all tests on code signal
+//    public static boolean isStrictlyIncreasing(int[] arr) {
+//        int modify = 0;
+//        int n = arr.length;
+//
+//        // Check whether the first element needs
+//        // to be modify or not
+//        if (arr[0] > arr[1]) {
+//            arr[0] = arr[1] / 2;
+//            modify++;
+//        }
+//
+//        // Loop from 2nd element to the 2nd last element
+//        for (int i = 1; i < n - 1; i++) {
+//
+//            // Check whether arr[i] needs to be modified
+//            if ((arr[i - 1] < arr[i] && arr[i + 1] < arr[i])
+//                    || (arr[i - 1] > arr[i] && arr[i + 1] > arr[i])) {
+//
+//                // Modifying arr[i]
+//                arr[i] = (arr[i - 1] + arr[i + 1]) / 2;
+//
+//                // Check if arr[i] is equal to any of
+//                // arr[i-1] or arr[i+1]
+//                if (arr[i] == arr[i - 1] || arr[i] == arr[i + 1])
+//                    return false;
+//
+//                modify++;
+//            }
+//        }
+//
+//        // Check whether the last element needs
+//        // to be modify or not
+//        if (arr[n - 1] < arr[n - 2])
+//            modify++;
+//
+//        // If more than 1 modification is required
+//        if (modify > 1)
+//            return false;
+//
+//        return true;
+//    }
 
-        // Check whether the first element needs
-        // to be modify or not
-        if (arr[0] > arr[1]) {
-            arr[0] = arr[1] / 2;
-            modify++;
-        }
+    //this one passes tests
+    //solution from https://github.com/RicheyHans/-JAVA-algorithm/blob/master/codefights/almostIncreasingSequence.java
+        public static boolean isStrictlyIncreasing(int[] sequence) {
+            //set up a counter, we can only modify the array once or less
+            int modify = 0;
+            for (int i = 0; i < sequence.length - 1; i++) {
+                System.out.println("sequence[i] " + sequence[i]);
+                System.out.println("sequence[i + 1] " + sequence[i + 1]);
 
-        // Loop from 2nd element to the 2nd last element
-        for (int i = 1; i < n - 1; i++) {
-
-            // Check whether arr[i] needs to be modified
-            if ((arr[i - 1] < arr[i] && arr[i + 1] < arr[i])
-                    || (arr[i - 1] > arr[i] && arr[i + 1] > arr[i])) {
-
-                // Modifying arr[i]
-                arr[i] = (arr[i - 1] + arr[i + 1]) / 2;
-
-                // Check if arr[i] is equal to any of
-                // arr[i-1] or arr[i+1]
-                if (arr[i] == arr[i - 1] || arr[i] == arr[i + 1])
-                    return false;
-
-                modify++;
+                if (sequence[i] - sequence[i+1] >= 0) {
+                    System.out.println("sequence[i] " + sequence[i]);
+                    System.out.println("sequence[i + 1] " + sequence[i + 1]);
+                    modify++;
+                    if (i - 1 >= 0 && i + 2 <= sequence.length - 1
+                            && sequence[i] - sequence[i+2] >= 0
+                            && sequence[i-1] - sequence[i+1] >= 0) {
+                        return false;
+                    }
+                }
+            }
+            if(modify <= 1) {
+                return true;
+            } else {
+                return false;
             }
         }
 
-        // Check whether the last element needs
-        // to be modify or not
-        if (arr[n - 1] < arr[n - 2])
-            modify++;
-
-        // If more than 1 modification is required
-        if (modify > 1)
-            return false;
-
-        return true;
-    }
-
-
 
     public static void main(String[] args) {
-        int[] numArr = {1, 3, 2, 1};
-        int[] numArr2 = {1, 3, 2};
-        int[] numArr3 = {1, 2, 3, 4};
-        System.out.println(isStrictlyIncreasing(numArr));  //false
-        System.out.println(isStrictlyIncreasing(numArr2)); //true
-        System.out.println(isStrictlyIncreasing(numArr3)); //true
+//        int[] numArr = {1, 3, 2, 1};
+//        int[] numArr2 = {1, 3, 2};
+//        int[] numArr3 = {1, 2, 3, 4};
+        int[] numArr4 = {1,1,2,3,4,4};
+//        System.out.println(isStrictlyIncreasing(numArr));  //false
+//        System.out.println(isStrictlyIncreasing(numArr2)); //true
+//        System.out.println(isStrictlyIncreasing(numArr3)); //true
+        System.out.println(isStrictlyIncreasing(numArr4)); //false
+
 
     }
 
