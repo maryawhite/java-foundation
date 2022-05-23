958f7658f6aac2bf4c41200d1c156d35ef6cb95d
Mary White <mary.a.white28@gmail.com>
2022-05-22 21:59:04 -0400

    bug: math is not correct for the pace

--- src/PaceCalculator.java
+++ src/PaceCalculator.java
@@ -1,4 +1,10 @@
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.logging.SimpleFormatter;
 
 public class PaceCalculator {
     //write a method that will calculate a total time based on pace per mile and distance
@@ -35,13 +41,24 @@ public class PaceCalculator {
         double answer;
         double fraction = convertFractionOfAMinute(seconds);
         double time = minutes + fraction;
-        System.out.println(time);
+        System.out.println("line 44 min + fraction " + time);
         answer = time/distance;
         String finalPace = DecimalFormat.getNumberInstance().format(answer);
-        String formatted = new DecimalFormat("#.0#").format(answer);
+        String formatted = new DecimalFormat("#.00").format(answer);
 
-        System.out.println(answer);
-        return "your pace " + finalPace + " per mile, formatted " + formatted;
+        System.out.println("line 49 time/distance " + answer);
+
+        //https://docs.oracle.com/javase/tutorial/i18n/format/decimalFormat.html
+        Locale currentLocale = Locale.getDefault();
+        DecimalFormatSymbols colonSymbol = new DecimalFormatSymbols(currentLocale);
+        colonSymbol.setDecimalSeparator(':');
+//        unusualSymbols.setGroupingSeparator(':');
+        String strange = "##0.00";
+        DecimalFormat colonFormatter = new DecimalFormat(strange, colonSymbol);
+//        weirdFormatter.setGroupingSize(4);
+        String withColon = colonFormatter.format(answer);
+
+        return "your pace " + finalPace + " per mile, formatted " + formatted + " with colon " + withColon;
     }
 
     public static double calculateDistance(double time, double pace) {
@@ -60,10 +77,31 @@ public class PaceCalculator {
 //        System.out.println(minToSec(8));   //8:00 = 480 seconds
 //        System.out.println(minToSeconds(8, 40));    //8:40 = 520 seconds
 //        System.out.println(minToSecondsAlt(8, 40));    //8:40 = 520 seconds
-        System.out.println(calculatePace(28, 32, 3.1));
-        System.out.println("distance " + calculateDistance(28.53, 9.20));
-        System.out.println("distance " + calculateDistance(30, 10.00));
+        System.out.println(convertFractionOfAMinute(32));
+        System.out.println(calculatePace(28, 32, 3.1));  //this is not correct should be 9:11, not 9:20
+//        System.out.println(calculatePace(18, 45, 3.1));
+        System.out.println(calculatePace(21, 32, 3.1));
+        System.out.println(calculatePace(21, 32, 3.1));
 
+        System.out.println("* " + 28.53/3.1);  //should be 9.11 pace, not 9.20
+        System.out.println("* " + 21.53/3.1);  //should be 6.56 pace, not 6.94
+        System.out.println(convertFractionOfAMinute(56));
+//        System.out.println(calculatePace(24, 45, 3.1));
+//        System.out.println("distance " + calculateDistance(28.53, 9.20));
+//        System.out.println("distance " + calculateDistance(30, 10.00));
+
+
+        //https://docs.oracle.com/javase/tutorial/i18n/format/decimalFormat.html
+//        Locale currentLocale = Locale.getDefault();
+//        DecimalFormatSymbols unusualSymbols = new DecimalFormatSymbols(currentLocale);
+//        unusualSymbols.setDecimalSeparator(':');
+////        unusualSymbols.setGroupingSeparator(':');
+//
+//        String strange = "##0.##";
+//        DecimalFormat weirdFormatter = new DecimalFormat(strange, unusualSymbols);
+////        weirdFormatter.setGroupingSize(4);
+//        String bizarre = weirdFormatter.format(12.67899999999);
+//        System.out.println(bizarre);
     }
 
 }
