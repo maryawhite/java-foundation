f19ffd9d95e3c4830a09f32e590f20fe116b6cba
Mary White <mary.a.white28@gmail.com>
2022-05-23 22:23:38 -0400

    clean up pace calculator

--- src/PaceCalculator.java
+++ src/PaceCalculator.java
@@ -1,10 +1,6 @@
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
-import java.text.Format;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Locale;
-import java.util.logging.SimpleFormatter;
 
 public class PaceCalculator {
     //write a method that will calculate a total time based on pace per mile and distance
@@ -27,26 +23,43 @@ public class PaceCalculator {
         return totalSeconds = minToSec(minutes) + seconds;
     }
 
-    public static double convertFractionOfAMinute(int seconds) {
-        return (double) seconds / 60;
+    public static float convertFractionOfAMinute(int seconds) {
+        return (float) seconds / 60;
     }
     //distance = time/pace
     //you run 3.1 miles in 28 minutes and 32 seconds. First, convert 32 seconds into a fraction of a minute (32 divided by 60 = .53 minutes). Added to 28 minutes = 28.53 minutes.
     //
     //28.53 minutes / 3.1 miles = 9.20 minutes per mile
 
-    public static String calculatePace(int minutes, int seconds, double distance) {
+
+    public static String calculatePace(int minutes, int seconds, float distance) {
         //first convert seconds to a fraction of a minute
         //then add it to minutes
-        double answer;
-        double fraction = convertFractionOfAMinute(seconds);
-        double time = minutes + fraction;
-        System.out.println("line 44 min + fraction " + time);
+        float answer;
+        float fraction = convertFractionOfAMinute(seconds);
+        float time = minutes + fraction;
+//        System.out.println("line 44 min + fraction " + time);
         answer = time/distance;
         String finalPace = DecimalFormat.getNumberInstance().format(answer);
-        String formatted = new DecimalFormat("#.00").format(answer);
+        String formatted = new DecimalFormat("#0.00").format(answer);
 
-        System.out.println("line 49 time/distance " + answer);
+//        System.out.println("line 49 time/distance " + answer);
+
+        //need to split it into a string so we can separate minutes and seconds...But only if there is a . (only if there are minutes and seconds. What if it's only seconds)
+        String[] strToSplit = formatted.split("\\.");
+//        System.out.println("split[0] " + strToSplit[0]);
+//        System.out.println("split[1] " + strToSplit[1]);
+        //convert back to a float
+        int min = Integer.parseInt(strToSplit[0]);
+        float sec = Float.parseFloat(strToSplit[1]);  //.20*60
+        float convertSec = (sec/100) * 60;
+        String formatConvertSec = new DecimalFormat("#").format(convertSec);
+        //write an if statement so something like 6:3 shows up as 6:03.
+        if(formatConvertSec.length() <= 1) {
+            formatConvertSec = "0" + formatConvertSec;
+        }
+//        System.out.println("*********************");
+//        System.out.println(min + ":" + formatConvertSec);
 
         //https://docs.oracle.com/javase/tutorial/i18n/format/decimalFormat.html
         Locale currentLocale = Locale.getDefault();
@@ -56,12 +69,18 @@ public class PaceCalculator {
         String strange = "##0.00";
         DecimalFormat colonFormatter = new DecimalFormat(strange, colonSymbol);
 //        weirdFormatter.setGroupingSize(4);
-        String withColon = colonFormatter.format(answer);
+//        String withColon = colonFormatter.format(answer);
+//        String withColon = colonFormatter.format(convertSec);
+//        System.out.println("with colon " + withColon);
 
-        return "your pace " + finalPace + " per mile, formatted " + formatted + " with colon " + withColon;
+
+//        return "your pace " + finalPace + " per mile, formatted " + formatted + " with colon " + withColon;
+        return "your pace " + min + ":" + formatConvertSec + " per mile";
+
     }
 
-    public static double calculateDistance(double time, double pace) {
+    public static float calculateDistance(float time, float pace) {
+        //user must enter their time and pace as a decimal, i.e 21.5, 7.5 for 21:30, 7:30.
         return time/pace;
     }
 
@@ -77,18 +96,25 @@ public class PaceCalculator {
 //        System.out.println(minToSec(8));   //8:00 = 480 seconds
 //        System.out.println(minToSeconds(8, 40));    //8:40 = 520 seconds
 //        System.out.println(minToSecondsAlt(8, 40));    //8:40 = 520 seconds
-        System.out.println(convertFractionOfAMinute(32));
-        System.out.println(calculatePace(28, 32, 3.1));  //this is not correct should be 9:11, not 9:20
-//        System.out.println(calculatePace(18, 45, 3.1));
-        System.out.println(calculatePace(21, 32, 3.1));
-        System.out.println(calculatePace(21, 32, 3.1));
+//        System.out.println(convertFractionOfAMinute(32));
 
-        System.out.println("* " + 28.53/3.1);  //should be 9.11 pace, not 9.20
-        System.out.println("* " + 21.53/3.1);  //should be 6.56 pace, not 6.94
-        System.out.println(convertFractionOfAMinute(56));
-//        System.out.println(calculatePace(24, 45, 3.1));
-//        System.out.println("distance " + calculateDistance(28.53, 9.20));
-//        System.out.println("distance " + calculateDistance(30, 10.00));
+//        System.out.println(calculatePace(28, 32, 3.107F));  // 9:11
+//        System.out.println(calculatePace(18, 45, 3.1F));  // 6:03
+//        System.out.println(calculatePace(21, 32, 3.1F));  // 6:57
+//        System.out.println(calculatePace(24, 0, 3.1F));  //7:45
+//        System.out.println(calculatePace(42, 30, 6.2F));  //6:51
+//        System.out.println(calculatePace(37, 30, 5F));  // 7:30
+//        System.out.println(calculatePace(12, 0, 1F));  // 12:00
+//        System.out.println(calculatePace(12, 0, .5F));  //24:00
+//        System.out.println(calculatePace(16, 0, 2F));  //8:00
+//        System.out.println(calculatePace(5, 0, 2F));   //2:30
+//        System.out.println(calculatePace(5, 0, 3F)); // 1:40/mile
+//        System.out.println(calculatePace(1, 0, 4F)); // 0:15
+//        System.out.println(calculatePace(0, 60, .25F)); // 4:00
+//        System.out.println(calculatePace(60, 60, 7F)); // 8:43
+//        System.out.println(calculatePace(120, 60, 7F)); // 17:17
+
+        System.out.println(calculateDistance(21.5F, 7.5F));
 
 
         //https://docs.oracle.com/javase/tutorial/i18n/format/decimalFormat.html
